e5e60a49796bb1322af05d872b5584a96e0be650
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-10-13 11:12:05 +0100

    feat: Implemented BaseMenuScreen.kt @Composable

---
+++ app/src/main/java/com/example/blazepizza/ui/BaseMenuScreen.kt
@@ -0,0 +1,18 @@
+package com.example.blazepizza.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BaseMenuScreen() {
+
+}
+
+@Composable
+fun MenuItemRow() {
+
+}
+
+@Composable
+fun MenuScreenButtonGroup() {
+
+}
