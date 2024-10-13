8a9db8cc4b5417a0c4fedffaa536ab937933910e
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-10-13 11:07:51 +0100

    feat: Implemented PizzaScreen.kt for AppBar

---
+++ app/src/main/java/com/example/blazepizza/PizzaScreen.kt
@@ -0,0 +1,24 @@
+package com.example.blazepizza
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+// TODO: Screen enum
+
+// TODO: AppBar
+
+@Composable
+fun PizzaApp() {
+    // TODO: Create Controller and intialization
+
+    // Create ViewModel
+
+    Scaffold(
+        topBar = {
+            // TODO: AppBar
+        }
+    ) { innerPadding ->
+
+        // TODO: Navigation host
+    }
+}
