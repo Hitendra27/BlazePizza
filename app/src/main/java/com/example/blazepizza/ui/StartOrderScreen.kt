2d96f0886f3ad1706edd7d6c94aa04e16ecc7aa3
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-10-13 11:52:43 +0100

    feat: Implemented StartOrderScreen with App images

--- app/src/main/java/com/example/blazepizza/ui/StartOrderScreen.kt
+++ app/src/main/java/com/example/blazepizza/ui/StartOrderScreen.kt
@@ -1,15 +1,68 @@
 package com.example.blazepizza.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.blazepizza.R
 
 @Composable
-fun StartOrderScreen() {
+fun StartOrderScreen(
+    onStartOrderButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.blazepizza),
+            contentDescription = "PizzaStartOrder Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(RoundedCornerShape(16.dp))
+        )
+        Image(
+            painter = painterResource(R.drawable.blazepizzalogo),
+            contentDescription = "PizzaStartOrder Pizza Logo",
+            modifier = Modifier
+                .size(200.dp)
+        )
+        Button(
+            onClick = onStartOrderButtonClicked,
+            Modifier.widthIn(min = 250.dp)
+        ) {
+            Text(stringResource(R.string.start_order))
+        }
+    }
 
 }
 
 @Preview(showSystemUi = true)
 @Composable
 fun StartOrderScreenPreview() {
-    StartOrderScreen()
+    StartOrderScreen(
+        onStartOrderButtonClicked = {},
+        modifier = Modifier
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .fillMaxSize()
+    )
 }
