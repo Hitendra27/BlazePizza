abc5a0c0a8bad77a43bcdd2c745b98cce82b2fdc
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-10-13 11:14:28 +0100

    feat: Implemented StartOrderScreen.kt & StartOrderScreenPreview

---
+++ app/src/main/java/com/example/blazepizza/ui/StartOrderScreen.kt
@@ -0,0 +1,15 @@
+package com.example.blazepizza.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun StartOrderScreen() {
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun StartOrderScreenPreview() {
+    StartOrderScreen()
+}
